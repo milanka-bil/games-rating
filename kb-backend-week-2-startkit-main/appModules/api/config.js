2e4d99cda520783f203f3ed272acd52f6e4e3684
milanka-bil <149702378+milanka-bil@users.noreply.github.com>
2024-05-19 18:13:19 +0300

    Add files via upload

---
+++ kb-backend-week-2-startkit-main/appModules/api/config.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://api-code-2.practicum-team.ru/games";
+
+const endpoints = {
+  game: `${BASE_URL}/game`,
+};
+
+module.exports = endpoints;
---
+++ kb-backend-week-2-startkit-main/controllers/index.js
@@ -0,0 +1,11 @@
+const mainRouteController = require("./cmain");
+const defaulRouteController = require("./default");
+const gameRouteController = require("./game");
+const voteRouteController = require("./vote");
+
+module.exports = {
+    mainRouteController,
+    defaulRouteController,
+    gameRouteController,
+    voteRouteController,
+};
