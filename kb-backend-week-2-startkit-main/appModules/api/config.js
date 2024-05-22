2b52c008a11123433c892036bd1f3fd905937b84
milanka-bil <149702378+milanka-bil@users.noreply.github.com>
2024-05-22 20:52:03 +0300

    добавила 3 часть дз

--- kb-backend-week-2-startkit-main/appModules/api/config.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "https://api-code-2.practicum-team.ru/games";
-
-const endpoints = {
-  game: `${BASE_URL}/game`,
-};
-
-module.exports = endpoints;
--- kb-backend-week-2-startkit-main/appModules/http-utils/index.js
+++
@@ -1,9 +0,0 @@
-const mimeTypes = require("./mime-types");
-const staticFile = require("./static-file");
-const parseBody = require("./parse-body");
-
-module.exports = {
-  mimeTypes,
-  staticFile,
-  parseBody,
-};
